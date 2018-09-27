04c6bb214739d8246d4db6f77dd7f2860d37c7ea
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-27 14:56:54 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/CLI/Wrapper/UnixCommand.pm
@@ -0,0 +1,62 @@
+package Acme::CPANModules::CLI::Wrapper::UnixCommand;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Various CLIs that wrap (popular) Unix commands",
+    description => <<'_',
+
+These CLI's usually are meant to be called as the Unix commands they wrap, e.g.:
+
+    alias ssh=sshwrap-hostcolor
+
+But they perform additional stuff.
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+        {
+            summary => 'Wraps ssh to remember the background terminal color of each user+host you went to',
+            module => 'App::sshwrap::hostcolor',
+            script => 'sshwrap-hostcolor',
+            'x.command' => 'ssh',
+        },
+        {
+            summary => 'Wraps man to search for (and tab-complete) Perl module documentation',
+            module => 'App::manwrap::pm',
+            script => 'manwrap-pm',
+            'x.command' => 'man',
+        },
+        {
+            summary => 'Wraps git to do additional stuff, e.g. set user+email automatically',
+            module => 'App::gitwrap',
+            script => 'gitwrap',
+            'x.command' => 'git',
+        },
+        {
+            summary => 'Wraps rsync to add color to output, particularly highlighting deletion',
+            module => 'App::rsynccolor',
+            script => 'rsynccolor',
+            'x.command' => 'rsync',
+        },
+        {
+            summary => 'Wraps rsync to check that source is newer than target',
+            module => 'App::rsync::new2old',
+            script => 'rsync-new2old',
+            'x.command' => 'rsync',
+        },
+        {
+            summary => 'Wraps cpanm to use local CPAN mirror and let you specify script name to install instead of module name',
+            module => 'App::lcpan',
+            script => ['lcpanm', 'lcpanm-script'],
+            'x.command' => 'cpanm',
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
